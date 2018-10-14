968c83bea3e46af9e7bc9bc959da3d7ce70854c1
hc <0903006293asd@gmail.com>
2018-10-14 16:33:26 -0700

    add

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
-from prompt_toolkit import prompt
+from PyInquirer import prompt
+from prompt_toolkit import prompt as p
+from pprint import pprint
 import requests
 import json
-import pprint
+import time
+
 class Log_in:
     def __init__(self):
         self.prompt_info()
@@ -9,8 +12,8 @@ class Log_in:
 
 
     def prompt_info(self):
-        self.account = prompt("Enter account: ")
-        self.password = prompt("Enter password: ", is_password = True)
+        self.account = p("Enter account: ")
+        self.password = p("Enter password: ", is_password = True)
     def log_in(self):
         data = {"apikey": "02646d3fb69a52ff072d47bf23cef8fd",
                 "client_id": "02646d3fb69a52ff072d47bf23cef8fd",
@@ -26,8 +29,8 @@ class Log_in:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         try:
             response = json.loads(requests.post("https://www.douban.com/service/auth2/token", data = data, headers = headers).text)
-            Log_in.access_token = "Bearer " + response["access_token"]
-            Log_in.expire = response["expires_in"]
+            self.access_token = "Bearer " + response["access_token"]
+            self.expire = response["expires_in"]
             self.username = response["douban_user_name"]
             print("Log in success!")
             print("Welcome!", self.username)
@@ -36,9 +39,10 @@ class Log_in:
             print("Log in fail")
 
 class Play_list:
-    url = "https://api.douban.com/v2/fm/app_channels"
-
+    channel_url = "https://api.douban.com/v2/fm/app_channels"
+    song_url = "https://api.douban.com/v2/fm/playlist"
     def __init__(self):
+        self.log_in = Log_in()
         data = {"alt": "json",
                 "app_name": "radio_iphone",
                 "apikey": "02646d3fb69a52ff072d47bf23cef8fd",
@@ -49,18 +53,85 @@ class Play_list:
                 "douban_udid": "b635779c65b816b13b330b68921c0f8edc049590",
                 "version": 115,
                 }
-        headers = {"Authorization": Log_in.access_token}
-        self.channel = json.loads(requests.get(Play_list.url, params = data, headers = headers ).text)['groups']
+        headers = {"Authorization": self.log_in.access_token}
+        try:
+            self.channel = json.loads(requests.get(Play_list.channel_url, params = data, headers = headers ).text)['groups']
+        except:
+            print("Fail to get channel")
 
     def group_select(self):
+        choices = list()
+        for idx in range(len(self.channel)):
+            if idx == 0:
+                choice = dict()
+                choice['name'] = "我的私人"
+                choice['value'] = self.channel[idx]['group_id']
+                choices.append(choice)
+            else:
+                choice = dict()
+                choice['name'] = self.channel[idx]['group_name']
+                choice['value'] = self.channel[idx]['group_id']
+                choices.append(choice)
+        question = [
+        {   "type": "list",
+            "name": "group",
+            "message": "Select a group",
+            "choices": choices
+        }
+                    ]
+        self.gselect = prompt(question)
 
-        self.select = int(prompt("Enter a group number."))
+
+
 
     def channel_select(self):
-        channel = self.channel[self.select]['chls']
-        for idx in range(len(channel)):
-            print(channel[idx]['name'])
-a\
+        channels = self.channel[self.gselect['group']]['chls']
+        while len(channels) == 0:
+            print(channels)
+            print("This group is empty")
+            time.sleep(0.5)
+            self.group_select()
+            channels = self.channel[self.gselect['group']]['chls']
+        choices = list()
+        for idx in range(len(channels)):
+            choice = dict()
+            choice['name'] = channels[idx]['name']
+            choice['value'] = channels[idx]['id']
+            choices.append(choice)
+
+        question = [
+        {
+        "type": "list",
+        "name": "channel",
+        "message": "Select a channel",
+        "choices": choices
+        }
+        ]
+
+        self.cselect = prompt(question)
+
+    def song(self):
+        headers = {"Authorization":self.log_in.access_token}
+        data = {
+            "channel": self.cselect['channel'],
+            "from": "mainsite",
+            "pt": "0.0",
+            "kbps": "128",
+            "formats": "aac",
+            "alt": "json",
+            "app_name": "radio_iphone",
+            "apikey": "02646d3fb69a52ff072d47bf23cef8fd",
+            "client": "s:mobile|y:iOS 10.2|f:115|d:b88146214e19b8a8244c9bc0e2789da68955234d|e:iPhone7,1|m:appstore",
+            "client_id": "02646d3fb69a52ff072d47bf23cef8fd",
+            "icon_cate": "xlarge",
+            "udid": "b88146214e19b8a8244c9bc0e2789da68955234d",
+            "douban_udid": "b635779c65b816b13b330b68921c0f8edc049590",
+            "version": "115",
+            "type": "n"
+        }
+        response = requests.get(self.song_url,headers=headers,params=data)
+        print(response.status_code)
+        self.cselect['channel']
 
 
 class Player:
@@ -68,7 +139,8 @@ class Player:
 
 
 if __name__ == '__main__':
-    login = Log_in()
+
     play_list = Play_list()
     play_list.group_select()
     play_list.channel_select()
+    play_list.song()
