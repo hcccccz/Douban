0dbf17146f8e02daba5e2784465847dc828ae01f
hc <0903006293asd@gmail.com>
2018-11-02 12:59:43 -0700

    add

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-class Dog:
-    def init(self):
-        self.bark = "wowowo"
-        self.barkk()
-    def barkk(self):
-        print(self.bark)
-dog = Dog()
-dog.init()
+class Color:
+
+    def __init__(self,rgb,name):
+        self.rgb = rgb
+        self.__name = name
+    def set_name(self,name):
+        if not isinstance(name,str):
+            raise TypeError("nane is not valid")
+        self.__name = name
+    def get_name(self):
+        return self.__name
+    def del_name(self):
+        del self.__name
+        print("del")
+    c = property(get_name,set_name,del_name)
+
+c = Color(1,"red")
+del c.c
